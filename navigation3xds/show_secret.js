33ce150d2b90435cc749e75a4f6be0613c5bf26e
Bohdan Misonh <46958633+raikoho@users.noreply.github.com>
2024-12-27 23:27:16 +0100

    Update show_secret.js

--- navigation3xds/show_secret.js
+++ navigation3xds/show_secret.js
@@ -9,13 +9,24 @@ function getRandomScript() {
     const randomIndex = Math.floor(Math.random() * scripts.length);
     return scripts[randomIndex];
 }
+
 // Завантаження випадкового скрипта
 function loadRandomScript() {
     const scriptPath = getRandomScript();
     const scriptElement = document.createElement('script');
     scriptElement.src = scriptPath;
     scriptElement.async = true;  // Запуск асинхронно, щоб не блокувати завантаження сторінки
+
+    // Додавання обробників подій для перевірки завантаження
+    scriptElement.onload = function() {
+        console.log(`Скрипт ${scriptPath} успішно завантажено`);
+    };
+    scriptElement.onerror = function() {
+        console.error(`Помилка при завантаженні скрипта: ${scriptPath}`);
+    };
+
     document.body.appendChild(scriptElement);
 }
+
 // Викликаємо функцію для завантаження випадкового скрипта при кожному оновленні сторінки
 loadRandomScript();
