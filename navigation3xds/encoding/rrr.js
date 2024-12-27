e994206147a1c1b650a5fe6781a4f8bc36664770
Bohdan Misonh <46958633+raikoho@users.noreply.github.com>
2024-12-27 23:16:43 +0100

    Create rrr.js

---
+++ navigation3xds/encoding/rrr.js
@@ -0,0 +1,4 @@
+// secret2.js
+const mysteryText = document.createElement('div');
+mysteryText.textContent = 'Секрет 2: Вітаємо! Це загадка: що буде, якщо помножити 2 на 3?';
+document.body.appendChild(mysteryText);
