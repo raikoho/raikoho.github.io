5ea6f1e3340914732ef8acd029993ac5acae70aa
Bohdan Misonh <46958633+raikoho@users.noreply.github.com>
2024-12-27 23:15:43 +0100

    Update show_secret.js

--- navigation3xds/show_secret.js
+++ navigation3xds/show_secret.js
@@ -1,1 +1,23 @@
+// Список доступних файлів, з яких вибиратиметься випадковий
+const scripts = [
+    'secrets/secret1.js',
+    'secrets/secret4.js'
+];
 
+// Функція для вибору випадкового скрипта
+function getRandomScript() {
+    const randomIndex = Math.floor(Math.random() * scripts.length);
+    return scripts[randomIndex];
+}
+
+// Завантаження випадкового скрипта
+function loadRandomScript() {
+    const scriptPath = getRandomScript();
+    const scriptElement = document.createElement('script');
+    scriptElement.src = scriptPath;
+    scriptElement.async = true;  // Запуск асинхронно, щоб не блокувати завантаження сторінки
+    document.body.appendChild(scriptElement);
+}
+
+// Викликаємо функцію для завантаження випадкового скрипта при кожному оновленні сторінки
+loadRandomScript();
