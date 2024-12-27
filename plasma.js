4fb098255cc589005118ef690392071903ad22f7
Bohdan Misonh <46958633+raikoho@users.noreply.github.com>
2024-12-27 21:20:04 +0100

    Update plasma.js

--- plasma.js
+++ plasma.js
@@ -1,56 +1,56 @@
-// Створення cфівфііфцапеитьорл ваамвігггггггггггггггггггггггг
+// Створення canvas
 const canvas = document.createElement('canvas');
 document.body.appendChild(canvas);
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
 
-// вавіаіваіва
+// Скрипт
 const particles = [];
+const centerX = canvas.width / 2;
+const centerY = canvas.height / 2;
 
-class SmokeParticle {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.radius = Math.random() * 10 + 5;
-        this.opacity = 1;
-        this.speedX = Math.random() * 2 - 1;
-        this.speedY = Math.random() * -2 - 1;
-        this.color = 'rgba(128, 128, 128, ';
+class BlackHoleParticle {
+    constructor() {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.speedX = (centerX - this.x) * 0.01;
+        this.speedY = (centerY - this.y) * 0.01;
+        this.radius = Math.random() * 3 + 1;
+        this.color = `hsl(${Math.random() * 360}, 50%, 50%)`;
     }
 
     draw() {
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-        ctx.fillStyle = this.color + this.opacity + ')';
+        ctx.fillStyle = this.color;
         ctx.fill();
     }
+
     update() {
         this.x += this.speedX;
         this.y += this.speedY;
-        this.opacity -= 0.01;
-        if (this.opacity <= 0) {
-            const index = particles.indexOf(this);
-            if (index > -1) particles.splice(index, 1);
+
+        if (Math.hypot(centerX - this.x, centerY - this.y) < 5) {
+            this.x = Math.random() * canvas.width;
+            this.y = Math.random() * canvas.height;
         }
+
         this.draw();
     }
 }
 
-function createSmoke(x, y) {
-    for (let i = 0; i < 5; i++) {
-        particles.push(new SmokeParticle(x, y));
+function createParticles() {
+    for (let i = 0; i < 100; i++) {
+        particles.push(new BlackHoleParticle());
     }
 }
 
-canvas.addEventListener('mousemove', (e) => {
-    createSmoke(e.clientX, e.clientY);
-});
-
-function animateSmoke() {
+function animateBlackHole() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     particles.forEach((particle) => particle.update());
-    requestAnimationFrame(animateSmoke);
+    requestAnimationFrame(animateBlackHole);
 }
 
-animateSmoke();
+createParticles();
+animateBlackHole();
