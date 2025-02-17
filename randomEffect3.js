3dc9901c0496d3a8d509b65ffbf217cb32177f5f
Bohdan Misonh <46958633+raikoho@users.noreply.github.com>
2025-02-17 22:58:13 +0100

    Update randomEffect3.js

--- randomEffect3.js
+++ randomEffect3.js
@@ -1,5 +1,5 @@
 // Випадковий вибір фону in index
-const effects = ['plasma2.js', 'textrain.js', 'rain.js', 'snow.js', 'snowWind.js', 'bloomstars.js', ' water.js'];
+const effects = ['textrain.js', 'rain.js', 'snow.js', 'snowWind.js', 'bloomstars.js'];
 const randomEffect = effects[Math.floor(Math.random() * effects.length)];
 const script = document.createElement('script');
 script.src = randomEffect;
